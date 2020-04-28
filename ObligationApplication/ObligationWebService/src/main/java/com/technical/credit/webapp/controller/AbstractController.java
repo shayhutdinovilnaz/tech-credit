7a6906a0945539aab87ec9bc1ccab30eb3c83578
Ilnaz Shayhutdinov <ilnaz-92@yandex.ru>
2020-04-28 14:05:43 +0300

    master - Logger is added.

--- ObligationApplication/ObligationWebService/src/main/java/com/technical/credit/webapp/controller/AbstractController.java
+++ ObligationApplication/ObligationWebService/src/main/java/com/technical/credit/webapp/controller/AbstractController.java
@@ -2,6 +2,8 @@ package com.technical.credit.webapp.controller;
 
 import com.technical.credit.obligationservice.exception.ModelNotFoundException;
 import com.technical.credit.webapp.formatter.RequestExceptionHandlerFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,15 +12,18 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public abstract class AbstractController {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractController.class);
 
     @ExceptionHandler(ModelNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(final ModelNotFoundException exception, final HttpServletRequest request) {
+        LOG.error("Something wrong is happened in request process. Required model was not found in system.", exception);
         final Map<String, Object> responseBody = RequestExceptionHandlerFormatter.format(exception, request, HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(final Exception exception, final HttpServletRequest request) {
+        LOG.error("Something wrong is happened in request process. Request", exception);
         final Map<String, Object> responseBody = RequestExceptionHandlerFormatter.format(exception, request, HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(responseBody, HttpStatus.INTERNAL_SERVER_ERROR);
     }
